ad0b724b6d2c9ccf17ee15de2c084d6a368d2f95
trefilmichal-prog <trefil.michal@gmail.com>
2026-02-03 21:17:06 +0100

    Add ticket decision buttons and persistence

--- src/persistence.js
+++ src/persistence.js
@@ -19,6 +19,7 @@ function getDefaultClanState() {
     clan_panel_configs: {},
     clan_clans: {},
     clan_applications: {},
+    clan_ticket_decisions: {},
     clan_ticket_vacations: {},
     clan_ticket_reminders: {},
     cooldowns: {},
